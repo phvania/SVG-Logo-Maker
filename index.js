796d0456072d29f986cf91537115cce2dcb8b7b0
phvania <129224910+phvania@users.noreply.github.com>
2023-09-07 17:32:53 -0400

    change

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//file system module import
 const fs = require('fs');
+//inquirer import
 const inquirer = require('inquirer');
 const { generateSVG, makeShape } = require("./lib/svg");
+//importing classes from ./lib/shapes directory
 const {Circle, Square, Triangle } = require("./lib/shapes");
 
-//const { default: Choices } = require('inquirer/lib/objects/choices');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 
 // this is where we will use inquirer to ask the user
@@ -11,6 +14,28 @@ const {Circle, Square, Triangle } = require("./lib/shapes");
 // we can implement our classes using that information
 // we'll implement a shape with color
 // then implement the svg based on that info
+
+// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
+
+class Svg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+
+    }
+    
+}
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -57,12 +82,12 @@ const questions = [
           console.log("Invalid user text, Please enter 1-3 Characters, no more and no less");
           return;
       }
-      console.log("User text: [" + user_text + "]");
+      //console.log("UserText: [" + userText + "]");
 
      // .then((answer) => {
     const userTextColor = answers.textColor;
       
-    const userShapeType = answers.logoShape;
+    const userShapeType = answers.logoShape;})
        // user_text_color = white
       //
         //console.log("wrong answer, ");
@@ -73,7 +98,7 @@ const questions = [
 
       //.then ((naswer) => {
 
-      })
+      
 
       
 
@@ -88,35 +113,53 @@ const questions = [
       //console.log("User entered shape = [" + user_shape_type + "]");
 
       //user shape
-
-      if(user_shape_type === "Square" || user_shape_type === "square") {
-       let   user_shape = 
-          user_shape = new Square(); console.log("User selected Square shape");
-      }
-      else if (user_shape_type === "Circle" || user_shape_type === "circle") {
-          user_shape = new Circle();
-          console.log("User selected Circle shape");
-      }
-      else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
-          user_shape = new Triangle();
-          console.log("User selected Triangle shape");
-      }
-      else {
-          console.log("Invalid shape!");
-      }
-      user_shape.setColor(user_shape_color);// Create a new Svg instance and add the shape and text elements to it
-      var svg = new Svg();
-      svg.setTextElement(user_text, user_font_color);
-      svg.setShapeElement(user_shape);
-      svgString = svg.render();
+      //let userShape;
+      //if(userShapeType === "Square" || userShapeType === "square") {
+      //userShape = new Square
+      //console.log("user selected  Square shape");
+     // } 
+     // else if (userShapeType === "Circle" || userShapeType === "circle") {
+        //  userShape = new Circle();
+         // console.log("User selected Circle shape");
+      //}
+      //else if (userShapeType === "Triangle" || userShapeType === "triangle") {
+         // userShape = new Triangle();
+         // console.log("User selected Triangle shape");
+      //}
+      //else {
+         // console.log("Invalid shape!");
+      //}
+      //const userShapeColor = answers.logoShape.color
+      //userShape.setColor(userShapeColor);// Create a new Svg instance and add the shape and text elements to it
+      //var svg = new Svg();
+      //svg.setTextElement(userText, userTextColor);
+      //svg.setShapeElement(userShape);
+      //svgString = svg.render();
       
       //Print shape to log
-      console.log("Displaying shape:\n\n" + svgString);
+      //console.log("Displaying shape:\n\n" + svgString);
       //document.getElementById("svg_image").innerHTML = svgString;
   
-     console.log("Shape generation complete!");
-      console.log("Writing shape to file...");
-      writeToFile(svg_file, svgString); 
+     //console.log("Shape generation complete!");
+      //console.log("Writing shape to file...");
+      //writeToFile(svg_file, svgString); 
+      
+      // TODO: Create a function to initialize app
+     // function init() {
+        //  inquirer
+         // .prompt(
+           //   questions
+         // ).then(data => {
+      //fs.writeFileSync('svg.file', generateMarkdown(data))
+          //  } )
+     // } 
+      
+      // Function call to initialize app
+     // init();  
+
+
+
+
   
   //init()
 
@@ -137,15 +180,40 @@ const questions = [
         // Something else went wrong
      // }
     ;// Function to write data to file
-    function writeToFile(svg_file, data) {
-        console.log("Writing [" + data + "] to file [" + svg_file + "]")
-        filesystem.writeFile(svg_file, data, function (err) {
+
+    function writeFile(logosvg, data) {
+        console.log("Writing [" + data + "] to file [" + logo.svg + "]")
+       filesystem.writeFile(logo.svg, data, function (err) {
             if (err) {
                 return console.log(err);
-            }
-            console.log(" Generated a logo.svg!");
+           }
+           console.log(" Generated a logo.svg!");
         });
     }
+    function init() {
+        inquirer
+        //.prompt(
+            questions
+        .then(data => {
+    fs.writeFileSync('logo.svg', 'generateSvg')
+          } )
+    }
+    
+    // Function call to initialize app
+   // init();
+    
+
+
+    
+    //fs.writeFile(fileName, svgString, (err) => {
+       // err ? console.log(err) : console.log("Generated logo.svg");
+     // });
+
+    //async function init() {
+       // console.log("Starting init");
+       // var svgString = "";
+        //var svg_file = "logo.svg";}
+
 
 
   
