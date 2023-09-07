d8512c0f5758b30b59502935846d549a1e5fcaaa
phvania <129224910+phvania@users.noreply.github.com>
2023-09-06 23:26:43 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# SVG-Logo-Maker
+## SVG-Logo-Maker##
+
+## License:
+  ![License: MIT]
+
+## Table of contents:
+ - [Description](#description)
+ - [Installation](##installation)
+ - [Usage](#usage)
+ - [Contribution](#Contribution)
+ - [Testing](#Testing) 
+
+ ## Demo:
+
+ ## Screenshot:
+
+ ## Description:
+ Build  an SVG logo using command line with your choice of shape, color and any three charactors og your choice. this generated SVG logo you can save as a .svg file.for this color keywords you can use hexadecimal numbers too.## Acceptance Criteria
+
+##  Acceptance criteria:
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+## Installation:
+
+Your application should use [Jest](https://www.npmjs.com/package/jest) for running the unit tests and [Inquirer](https://www.npmjs.com/package/inquirer/v/8.2.4) for collecting input from the user. The application will be invoked by using the following command: on git bash, 'node index.js  use the terminal 'npm install to install dependecies.and use the command'npm install --save-dev jest to install jest as a devdependency.
+
+## Usage:
+You will use command line to use this application, node.js  installed by typing 'node -v', then 'node index.js', use inquirer for series of questions by prompt, follow up all question, select shape and color to your choise and choose three charactors for logo.once all prompts will be answered new logo will be generated with the file name logo.svg.follow the steps in the demo link.
+
+## Contribution;
+Below of this page, there are my contact information provided, you cantact me for more ideas or requests.
+
+## Testing:
+The application must include `Triangle`, `Circle`, and `Square` classes, as well as tests for each of these classes using Jest. While not a requirement, it is recommended that you place any common functionality and properties shared by the `Triangle`, `Circle`, and `Square` classes in a parent `Shape` class and use inheritance to reuse the code in the child classes.
+
+##  Questions:
+If you have any questions about the repo,you  can contact me at fewcwfe
+You can find more of my work at [](https://github.com//)
+
+##
+Github: phvania
+Email: vaniapriti@gmail.com
+
+## Helpful SVG Resources
+
+* [Example SVG](https://static.fullstack-bootcamp.com/fullstack-ground/module-10/circle.svg)
+
+* [Scalable Vector Graphics (SVG)](https://en.wikipedia.org/wiki/Scalable_Vector_Graphics)
+
+* [SVG tutorial](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial)
+
+* [Basic SVG shapes](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes)
+
+* [Text in SVG](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Texts)
+
+* [SVG VS Code extension](https://marketplace.visualstudio.com/items?itemName=jock.svg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,145 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { generateSVG, makeShape } = require("./lib/svg");
+const {Circle, Square, Triangle } = require("./lib/shapes");
+
+//const { default: Choices } = require('inquirer/lib/objects/choices');
+
+
+// this is where we will use inquirer to ask the user
+// which shape, color and text they want
+// we can implement our classes using that information
+// we'll implement a shape with color
+// then implement the svg based on that info
+
+// TODO: Create an array of questions for user input
+const questions = [
+    //logo text
+    {
+        type:"input",
+        message:"please select up to  three charactors for your  logo text",
+        name:"text"
+    },
+    //text  color
+    {
+        type:"input",
+        message:"please enter a color keyword or hexadecimal number for your text color",
+        name:"textColor"
+    },
+    // options for shapes 
+    {
+        type:"list",
+        message:"please choose  logo shape (hit enter to select)",
+        name: "logoShape",
+        choices: ['Triangle', 'Circle', 'Square']
+     
+    },
+    //shape color
+    {
+       type:"input" ,
+       message:"please enter a color keyword or hexadecimal number for your logo's color",
+       name:"logoShapeColor"
+    },
+    ]
+    inquirer
+    .prompt(
+      /* Pass your questions in here */
+      questions
+    )
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+      	var user_text = "";
+      if (answers.text.length > 0 && answers.text.length < 4) {
+          // 1-3 chars, currect answer
+          user_text = answers.text;
+      } else {
+          // 0 or 4+ chars, wrong answer
+          console.log("Invalid user text, Please enter 1-3 Characters, no more and no less");
+          return;
+      }
+      console.log("User text: [" + user_text + "]");})
+
+      var user_text_color = "";
+      if(answer.text.color, white){
+        user_text_color = white
+      } else {
+        console.log("wrong answer, ");
+        return;
+
+        console.log("user text color;white")
+      }
+      
+      
+
+      //user font color
+      user_text_color = answers["text-color"];
+      console.log("User text color: [" + user_text_color + "]");
+      //user shape color
+      user_shape_color = answers.logoshape;
+      console.log("User shape color: [" + user_shape_color + "]");
+      //user shape type
+      user_shape_type = answers["pixel-image"];
+      console.log("User entered shape = [" + user_shape_type + "]");
+
+      //user shape
+      let user_shape;
+      if (user_shape_type === "Square" || user_shape_type === "square") {
+          user_shape = new Square();
+          console.log("User selected Square shape");
+      }
+      else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+          user_shape = new Circle();
+          console.log("User selected Circle shape");
+      }
+      else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+          user_shape = new Triangle();
+          console.log("User selected Triangle shape");
+      }
+      else {
+          console.log("Invalid shape!");
+      }
+      user_shape.setColor(user_shape_color);// Create a new Svg instance and add the shape and text elements to it
+      var svg = new Svg();
+      svg.setTextElement(user_text, user_font_color);
+      svg.setShapeElement(user_shape);
+      svgString = svg.render();
+      
+      //Print shape to log
+      console.log("Displaying shape:\n\n" + svgString);
+      //document.getElementById("svg_image").innerHTML = svgString;
+  
+      console.log("Shape generation complete!");
+      console.log("Writing shape to file...");
+      writeToFile(svg_file, svgString); 
+  
+  init()
+
+  
+
+
+  
+
+
+    
+      
+
+    
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });// Function to write data to file
+    function writeToFile(fileName, data) {
+        console.log("Writing [" + data + "] to file [" + fileName + "]")
+        filesystem.writeFile(fileName, data, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(" Generated a logo.svg!");
+        });
+    }
+
+
+  
