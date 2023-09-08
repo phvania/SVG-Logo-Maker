0ef6c14fc10fed5f9a9e249af45e15ffbafa2895
phvania <129224910+phvania@users.noreply.github.com>
2023-09-07 21:47:55 -0400

    update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const fs = require('fs');
 //inquirer import
 const inquirer = require('inquirer');
-const { generateSVG, makeShape } = require("./lib/svg");
+const SVG  = require("./lib/svg");
 //importing classes from ./lib/shapes directory
 const {Circle, Square, Triangle } = require("./lib/shapes");
 
-const { default: Choices } = require('inquirer/lib/objects/choices');
+//const { default: Choices } = require('inquirer/lib/objects/choices');
 
 
 // this is where we will use inquirer to ask the user
@@ -17,28 +17,30 @@ const { default: Choices } = require('inquirer/lib/objects/choices');
 
 // Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
 
-class Svg{
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
-    }
-    render(){
+//class Svg{
+    //constructor(){
+        //this.textElement = ''
+       // this.shapeElement = ''
+   // }
+    //render(){
 
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
-    }
-    setTextElement(text,color){
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
-    setShapeElement(shape){
-        this.shapeElement = shape.render()
+        //return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    //}
+   // setTextElement(text,color){
+       // this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    //}
+    //setShapeElement(shape){
+        //this.shapeElement = shape.render()
 
-    }
+///}
     
-}
+//}
 
 
 // TODO: Create an array of questions for user input
-const questions = [
+function promptUser() {
+inquirer 
+.prompt( [
     //logo text
     {
         type:"input",
@@ -55,7 +57,7 @@ const questions = [
     {
         type:"list",
         message:"please choose  logo shape (hit enter to select)",
-        name: "logoShape",
+        name: "Shape",
         choices: ['Triangle', 'Circle', 'Square']
      
     },
@@ -63,31 +65,34 @@ const questions = [
     {
        type:"input" ,
        message:"please enter a color keyword or hexadecimal number for your logo's color",
-       name:"logoShapeColor"
+       name:"ShapeBackgroundColor"
     },
-    ]
-    inquirer
-    .prompt(
-      /* Pass your questions in here */
-      questions
-    )
-    .then((answers) => {
+    ])
+};
+        
+        
+
+       .then((answers) => {
       // Use user feedback for... whatever!!
     
       if (answers.text.length > 0 && answers.text.length < 4) {
           // 1-3 chars, currect answer
-         const userText = answers.text;
+         const userText = answers.text;        
+           console.log("Invalid user text, Please enter 1-3 Characters, no more and no less");
+         promptUser();
+
       } else {
-          // 0 or 4+ chars, wrong answer
-          console.log("Invalid user text, Please enter 1-3 Characters, no more and no less");
-          return;
-      }
+          //calling write file function to generate svg file
+          writeToFile("logo.svg", answers);
+          
+        
+      }})
       //console.log("UserText: [" + userText + "]");
 
      // .then((answer) => {
-    const userTextColor = answers.textColor;
+   // const userTextColor = answers.textColor;
       
-    const userShapeType = answers.logoShape;})
+    //const userShapeType = answers.logoShape;})
        // user_text_color = white
       //
         //console.log("wrong answer, ");
@@ -111,13 +116,61 @@ const questions = [
       //user shape type
       //user_shape_type = answers["pixel-image"];
       //console.log("User entered shape = [" + user_shape_type + "]");
+      // File starts as an empty string
+  let svgString = "";
+  // Sets width and height of logo container
+  svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
+  svgString += "<g>";
+  // Takes user input for shape choice and inserts it into SVG file
+  svgString += `${answers.shape}`;
 
-      //user shape
+
+
+
+
+
+
+
+
+
+      let  shape;
+      if ( answer.logoShape === "Triangle") {
+        shape = new Triangle();
+       svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+      } 
+      else  
+      
+      if ( answer.logoShape === "Cricle") {
+        shape = new Circle(); 
+       svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+        
+      }
+      else
+      
+      if ( answer.logoShape === "Square") {
+        shape = new Square();  
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`
+     
+      }  // <text> tag gives rise to text alignment, text-content/text-color taken in from user prompt and gives default font size of "40"
+      svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+      // Closing </g> tag
+      svgString += "</g>";
+      // Closing </svg> tag
+      svgString += "</svg>";
+
+    
+
+
+
+
+      
       //let userShape;
       //if(userShapeType === "Square" || userShapeType === "square") {
       //userShape = new Square
       //console.log("user selected  Square shape");
-     // } 
+      //} 
      // else if (userShapeType === "Circle" || userShapeType === "circle") {
         //  userShape = new Circle();
          // console.log("User selected Circle shape");
@@ -131,8 +184,8 @@ const questions = [
       //}
       //const userShapeColor = answers.logoShape.color
       //userShape.setColor(userShapeColor);// Create a new Svg instance and add the shape and text elements to it
-      //var svg = new Svg();
-      //svg.setTextElement(userText, userTextColor);
+      //var svg = new svg();
+     // svg.setTextElement(userText, userTextColor);
       //svg.setShapeElement(userShape);
       //svgString = svg.render();
       
@@ -164,6 +217,11 @@ const questions = [
   //init()
 
   
+  // Using file system module to generate svg file, takes in file name given in the promptUser function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
+
 
 
   
@@ -181,7 +239,7 @@ const questions = [
      // }
     ;// Function to write data to file
 
-    function writeFile(logosvg, data) {
+    function writeFile(logo, data) {
         console.log("Writing [" + data + "] to file [" + logo.svg + "]")
        filesystem.writeFile(logo.svg, data, function (err) {
             if (err) {
@@ -190,15 +248,15 @@ const questions = [
            console.log(" Generated a logo.svg!");
         });
     }
-    function init() {
-        inquirer
+    //function init() {
+       // inquirer
         //.prompt(
-            questions
-        .then(data => {
-    fs.writeFileSync('logo.svg', 'generateSvg')
-          } )
-    }
-    
+           // questions
+//.then(data => {
+    //fs.writeFileSync('logo.svg', 'generateSvg')
+         // } )
+   // }
+    module.exports = SVG
     // Function call to initialize app
    // init();
     
